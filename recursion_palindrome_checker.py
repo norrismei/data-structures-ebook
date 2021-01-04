30fed343464686023e07251e34fb4ce804264157
Norris Mei <norrismei3@gmail.com>
2021-01-04 15:41:01 -0800

    Completed fxn for checking palindrome with no spaces or special chars

--- recursion_palindrome_checker.py
+++ recursion_palindrome_checker.py
@@ -1,14 +1,22 @@
-from test import testEqual
+# from test import testEqual
 
 def remove_white(s):
     return s
 
 def is_pal(s):
-    return False
+    if len(s) <=1:
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_pal(s[1:-1])
+        else:
+            return False
 
-testEqual(is_pal(remove_white("x")), True)
-testEqual(is_pal(remove_white("radar")), True)
-testEqual(is_pal(remove_white("hello")), False)
-testEqual(is_pal(removeWremove_whitehite("")), True)
-testEqual(is_pal(remove_white("hannah")), True)
-testEqual(is_pal(remove_white("madam i'm adam")), True)
+print(is_pal("hannah"))
+
+# testEqual(is_pal(remove_white("x")), True)
+# testEqual(is_pal(remove_white("radar")), True)
+# testEqual(is_pal(remove_white("hello")), False)
+# testEqual(is_pal(removeWremove_whitehite("")), True)
+# testEqual(is_pal(remove_white("hannah")), True)
+# testEqual(is_pal(remove_white("madam i'm adam")), True)
